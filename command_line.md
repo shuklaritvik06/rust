20697d306dc44108ad72705d2341ebd329caa959
Ritvik Shukla <ritvikshukla261@gmail.com>
2022-12-12 16:44:09 +0530

    init

---
+++ command_line.md
@@ -0,0 +1,4 @@
+# Commands that are used in the Rust Project Configuration
+
+- cargo run hello
+- hello word file
--- learning/src/arrays.rs
+++ learning/src/arrays.rs
@@ -1,11 +1,11 @@
-use std::mem;
+// use std::mem;
 pub fn array(){
     let array : [i32; 4] = [0,1,2,3];
     println!("{}",array[0]);
     println!("{}",array.len());
     println!("{:?}",array);
     // println!("{}",std::mem::size_of_val(&array));
-    println!("{}",mem::size_of_val(&array));
+    // println!("{}",mem::size_of_val(&array));
     // Get Slice of an Array
     let slice : &[i32] = &array[0..2];
     println!("{:?}",slice);
